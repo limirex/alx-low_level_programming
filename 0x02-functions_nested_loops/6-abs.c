d16c13187d614b0824377d4497d82b78c83ef7b2
limirex <livingstonechangko@gmail.com>
2022-09-13 18:51:54 -0700

    absolute number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - compute absolute value
+ * @n: int to compute as absolute value
+ *
+ * return: absolute value
+ */
+
+int _abs(int)
+{
+if (n < 0)
+{
+return (-n);
+}
+else
+{
+return (n);
+}
+}
