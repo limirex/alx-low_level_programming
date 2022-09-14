b1655c97693c4635e04c8588a7f72705a825fc81
limirex <livingstonechangko@gmail.com>
2022-09-13 17:30:46 -0700

    is alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _islower - returns 1 if char is lowercase
+ * _isalpha - returns 1 if char is lowercase or uppercase
  *
- * @c: character to be checked if lower case, type int
+ * @c: character to be checked if lower case/uppercase, type int
  *
- * Return: 1 if lower case, 0 otherwise
+ * Return: 1 if lower case/upper case, 0 otherwise
  **/
 
 int _isalpha(int c)
