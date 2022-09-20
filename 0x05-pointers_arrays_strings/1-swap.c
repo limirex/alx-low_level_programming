02888b3b25ad30fbf6cd75da3afc8c71d7c78293
limirex <livingstonechangko@gmail.com>
2022-09-19 17:54:30 -0700

    swap a&b

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - swap the values of two integers
+ * @a: first value
+ * @b: second value
+ *
+ * Return: nothing
+ **/
+
+void swap_int(int *a, int *b)
+{
+*a = b;
+*b = a;
+}
