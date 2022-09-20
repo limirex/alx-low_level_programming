59daa8dc7bd298f6e8049a51e300f77a434b8e72
limirex <livingstonechangko@gmail.com>
2022-09-19 17:45:04 -0700

    update to n

---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+All my tasks for the pointers, arrays and strings for C are located here
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,7 @@
+#include <unistd.h>
+/**
+ * int _putchar - writes _putchar to stdout
+ **/
+
+int _putchar(char);
+void reset_to_98(int *n);
