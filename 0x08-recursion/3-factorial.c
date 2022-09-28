32272f6ecd1377f6f6c3c30385e67c0127921b46
limirex <livingstonechangko@gmail.com>
2022-09-28 11:42:28 -0700

    factorial of a given number

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * factorial - gets the factorial of a given number
+ * @n - number given
+ *
+ * Return: factorial of n
+ */
+
+int factorial(int n)
+{
+if (n < 0)
+return (-1);
+
+if (n == 0)
+{
+return (1);
+}
+else
+{
+n = (n * factorial(n-1));
+return (n);
+}
+}
