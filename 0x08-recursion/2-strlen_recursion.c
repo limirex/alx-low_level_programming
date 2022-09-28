e9142bb4e17e9c7a5408a6d43b84452f71c37f36
limirex <livingstonechangko@gmail.com>
2022-09-28 11:25:53 -0700

    string length with recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - calculates the length of a string
+ * @s: string
+ *
+ * Return: string length
+ */
+
+int _strlen_recursion(char *s)
+{
+if (s[0] == '\0')
+return (0);
+
+if (s[1] == '\0')
+{
+return (1);
+}
+else
+{
+return (_strlen_recursion(&s[1]) + 1);
+}
+}
