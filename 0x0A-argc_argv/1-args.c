9608a530bc63543bd2d091a2b9e0d4914901a59a
limirex <livingstonechangko@gmail.com>
2022-09-30 17:26:44 -0700

    number of arguments

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,14 +8,14 @@
  * Description: print all arguments including file name
  * Return: zero
  **/
-
 int main(int argc, char *argv[])
+  
 {
 int i;
 
-for (i = 0; i < argc; i++)
+for(i = 0; i < argc; i++)
 {
-printf("%s\n", argv[i]);
+printf("argv[%d] = %s\n", i, argv[i]);
 }
-return (0);
+return (0); 
 }
